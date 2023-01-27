ab444a1a6b60289177426068e280ccd5fbeaf765
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-01-26 23:49:19 -0500

    Add home route and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormularioComponent } from './formulario/formulario.component';
 import { GaleriaComponent } from './galeria/galeria.component';
+import { InformacionComponent } from './informacion/informacion.component';
 import { ReporteUsuarioComponent } from './reporte-usuario/reporte-usuario.component';
 import { ReporteComponent } from './reporte/reporte.component';
 
 const routes: Routes = [
+  {path: '', component:InformacionComponent},
   {path:'informacion', component:GaleriaComponent},
   {path:'registro', component:FormularioComponent},
   {path:'reporte', component:ReporteComponent},
